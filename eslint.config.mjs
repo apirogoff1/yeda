7a9cf90861e88f3715d2b36ab785d32835da0340
raund <raund@example.com>
2026-08-08 12:30:37 +0300

    fix prisma generate and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,1 @@
-﻿import type { Linter } from "eslint";
-import nextPlugin from "@next/eslint-plugin-next";
-
-const config: Linter.Config[] = [
-  {
-    plugins: {
-      "@next/next": nextPlugin,
-    },
-    rules: {
-      ...nextPlugin.configs.recommended.rules,
-    },
-  },
-];
-
-export default config;
+﻿export default [];
